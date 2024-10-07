9e531a5c756ee60449c3c031d7d01899e95cd989
chrischiedo <8336086+chrischiedo@users.noreply.github.com>
2024-10-07 23:03:45 +0300

    clean up main file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-// mod lexer;
-
-// use std::io;
-
-// use crate::lexer::tokenize;
-
 fn main() {
     rustyscm::repl();
 }
-
-// fn main() {
-//     let mut input = String::new();
-//
-//     io::stdin()
-//         .read_line(&mut input)
-//         .expect("Failed to read line");
-//
-//     let tokens = tokenize(&*input);
-//
-//     println!("Tokens: {:?}", tokens);
-// }
